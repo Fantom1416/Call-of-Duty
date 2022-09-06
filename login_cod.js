ccf79f55b450464fcade40090dc30546f9d63a98
Fantom1416 <barnoyusupova102@example.com>
2022-09-06 05:27:01 -0700

    first commit

--- login_cod.js
+++ login_cod.js
@@ -7,3 +7,18 @@
 //         alert("Pustoy")
 //     }
 // }
+
+
+console.log(localStorage)
+
+const Input1 = document.querySelector("#input")
+const Input2 = document.querySelector("#input2")
+
+
+function Signin(e) {
+console.log(Input1.value)
+console.log(Input2.value)
+
+localStorage.setItem("name", "Bobir")
+localStorage.setItem("Lastname", "Temirov")
+}
